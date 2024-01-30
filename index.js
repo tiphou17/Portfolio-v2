71e7fc9bd221124d63c28366c9bc1b8a686d543b
Tiphaine Fontenil <tiphaine.fontenil@gmail.com>
2024-01-30 14:15:03 +0100

    30.01.2024 version

--- index.js
+++ index.js
@@ -17,3 +17,29 @@ closeButton.addEventListener('click', () => {
     menuList.style.animationPlayState = "running";
 
 })
+
+//barre de techno
+var technolist = [
+    {nom : 'html', achievementPercentage : 0.8},
+    {nom : 'css', achievementPercentage : 0.6},
+    {nom : 'php', achievementPercentage : 0.1},
+    {nom : 'javascript', achievementPercentage : 0.5},
+    {nom : 'python', achievementPercentage : 0.3}
+];
+
+//le stroke correspond au périmètre du cercle (pi * 2 * r)
+technolist.forEach((techno)=>{
+    var percentToStroke = 218-(techno.achievementPercentage)*218
+    document.getElementsByClassName(techno.nom)[0].style.setProperty('--strokeVar', percentToStroke)
+})
+
+
+document.getElementById('techno-logo').addEventListener("mouseover",()=>{
+    document.getElementById('techno-progress-bar').style.display = 'flex';
+})
+
+document.getElementById('techno-progress-bar').addEventListener("mouseout",()=>{
+    document.getElementById('techno-progress-bar').style.display = 'none';
+})
+
+
---
+++ reaction/reaction.js
@@ -0,0 +1,86 @@
+// jeu 
+
+var countElement = document.getElementById('count');
+var startButton = document.getElementById('startButton');
+var clickButton = document.getElementById('clickButton');
+var againButton = document.getElementById('againButton');
+var reactionCount = document.getElementById('reactionCount');
+var reactionContainer = document.getElementById('reaction-container');
+var reactionTime = 0;
+var startTime = 0;
+var stopTime = 0;
+var count = 4;
+
+console.log(window.innerHeight);
+document.getElementById('game').style.height = (window.innerHeight -20)+'px';
+
+function startReaction(){
+    startTime =  new Date();
+
+}
+
+function stopReaction(){
+    stopTime = new Date();
+
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+
+
+
+function interval(){
+    var timeout = setTimeout(interval, 1000)
+    count -- 
+    countElement.textContent = count;
+    if (count == 0){
+        clearTimeout(timeout);
+        countElement.style.display = 'none';
+        clickButton.style.display = 'block';
+        startReaction()
+        //je prends la taille de la fenetre afin d'éviter que mon bouton sorte de la fenetre
+        var windowWidth = window.innerWidth;
+        var windowHeight = window.innerHeight;
+        //je mets une marge aléatoire sur mon bouton
+        var x = getRndInteger(20,(windowWidth-30));
+        var y = getRndInteger(20,(windowHeight-30));
+        clickButton.style.margin = `${y}px ${x}px`;
+
+    }
+}
+
+// var failTimeout = setTimeout(() => {
+//         reactionContainer.style.display = 'block';
+//         clickButton.style.display = 'none';
+//         reactionContainer.textContent = 'Vous êtes trop lents !!!';
+//         againButton.style.display = 'block';
+//     }, 6000);
+
+startButton.addEventListener('click',function(){
+    interval();
+    countElement.style.display = 'block';
+    startButton.style.display = 'none';
+  
+})
+
+clickButton.addEventListener('click', function(){
+    stopReaction();
+    reactionTime = (stopTime - startTime)/1000;
+    reactionContainer.textContent = 'Votre temps de réaction est de ' + reactionTime + ' secondes'
+    clickButton.style.display = 'none';
+    reactionContainer.style.display = 'block';
+    againButton.style.display = 'block';
+} )
+
+againButton.addEventListener('click', function(){
+    count = 4;
+    interval();
+    countElement.style.display = 'block';
+    againButton.style.display = 'none';
+    reactionContainer.style.display = 'none';
+    
+})
+
+
